87ead72e4348e66c3f65772930b6d69e655fa931
madan96 <rishabhmadan96@gmail.com>
2016-10-16 16:27:25 +0530

    Resolved indentation problem

--- dc_hub.py
+++ dc_hub.py
@@ -4,13 +4,13 @@ import os
 
 
 def get_hub_add():
-	try: 
+	try : 
         graph = GraphAPI(os.environ['FB_ACCESS_TOKEN'])
         query = "hhfhkgp?fields=website"
         hub_website = graph.get(query)['website']
         x = re.search(r'\d+\.\d+\.\d+\.\d+', hub_website)
         return x.group()
-    except:
+    except :
         error_msg = "Got following error in getting upcoming events :\n{}".format(traceback.format_exc())
         slack_notification(error_msg)    
 
