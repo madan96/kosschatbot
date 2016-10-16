c8471b5a51e8290caab72e795ed8770be50859a8
madan96 <rishabhmadan96@gmail.com>
2016-10-16 16:23:24 +0530

    DC error catch

--- dc_hub.py
+++ dc_hub.py
@@ -4,8 +4,13 @@ import os
 
 
 def get_hub_add():
-    graph = GraphAPI(os.environ['FB_ACCESS_TOKEN'])
-    query = "hhfhkgp?fields=website"
-    hub_website = graph.get(query)['website']
-    x = re.search(r'\d+\.\d+\.\d+\.\d+', hub_website)
-    return x.group()
+	try: 
+        graph = GraphAPI(os.environ['FB_ACCESS_TOKEN'])
+        query = "hhfhkgp?fields=website"
+        hub_website = graph.get(query)['website']
+        x = re.search(r'\d+\.\d+\.\d+\.\d+', hub_website)
+        return x.group()
+    except:
+        error_msg = "Got following error in getting upcoming events :\n{}".format(traceback.format_exc())
+        slack_notification(error_msg)    
+
